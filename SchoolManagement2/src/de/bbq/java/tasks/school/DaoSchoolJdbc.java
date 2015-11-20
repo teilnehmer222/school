7abb59d17a2dae57d881dc4b8ce51ed18289123b
Thorsten2201 <eybethorsten@gmail.com>
2015-11-20 09:42:55 +0100

    Lost classes

---
+++ SchoolManagement2/src/de/bbq/java/tasks/school/DaoSchoolJdbc.java
@@ -0,0 +1,59 @@
+package de.bbq.java.tasks.school;
+
+public class DaoSchoolJdbc extends DaoSchoolAbstract {
+
+	@Override
+	public boolean saveElement(CourseDF course) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveElement(TeacherDF teacher) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveElement(StudentDF student) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadElement(CourseDF course) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadElement(TeacherDF teacher) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadElement(StudentDF student) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteElement(CourseDF course) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteElement(TeacherDF teacher) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteElement(StudentDF student) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
